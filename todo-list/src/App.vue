d4ab0a3c4d0247cb44afd83fa7eee3f4b7e4498d
Stolyar100 <olejer555@gmail.com>
2020-03-03 08:10:17 +0200

    Added TodoList component with input and list of todos

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -1,28 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <img alt="Vue logo" src="./assets/logo.png" class="logo">
+    <todo-list />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoList from './components/TodoList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TodoList, 
   }
 }
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+
+.container {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: flex-start;
+
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.logo {
+  
 }
 </style>
---
+++ todo-list/src/components/TodoList.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="container">
+    <h1>ToDo list go here omg</h1>
+    <input type="text" class="todo-input" placeholder="What needs to be done" v-model="newTodo" @keyup.enter="addTodo">
+    <div v-for="todo in todos" :key="todo.id" class="todo-item">{{ todo.title}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todo-list',
+  props: {
+  },
+
+  data: function () {
+    return {
+      newTodo: '',
+      idForTodo: 3,
+      todos: [
+        {
+          'id': 1,
+          'title': 'Finish Vue Screentest',
+          'completed': false,
+
+        },
+        {
+          'id': 2,
+          'title': 'Read Vue documentation',
+          'completed': false,
+          
+        }
+      ]
+    }
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        id: this.idForTodo,
+        title: this.newTodo,
+        completed:false
+      })
+
+      this.newTodo = ''
+
+      this.idForTodo++ 
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .todo-input {
+    width: 100%;
+    padding: 10px 18px;
+    font-size: 18px;
+    margin-bottom: 16px;
+    
+  }
+
+  .todo-input:focus {
+    outline: 0;
+  }
+
+</style>
